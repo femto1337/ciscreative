c33ea492f2b0d46b158b4e2286d176f5a78bbf42
femto1337 <51991097+femto1337@users.noreply.github.com>
2020-01-20 21:47:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,7 +92,7 @@ async def close(ctx):
         await ctx.author.send('Вы не искали боксфайт.')
 
 @client.event
-async def on_raw_reaction_clear(payload):
+async def on_raw_reaction_remove(payload):
     guild = discord.utils.find(lambda g: g.id == payload.guild_id, client.guilds)
     user = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
     print('on_raw_reaction_clear')
